6c16aefdc63c579e994459cb2e342631260bf042
Harry Sohal <meghna0593@gmail.com>
2024-04-05 11:37:18 -0700

    add maps impl

---
+++ maps/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// First way
+	// colors := map[string]string{
+	// 	"red":   "#ff0000",
+	// 	"green": "#4bf745",
+	// }
+
+	// Second way
+	// var colors map[string]string
+
+	// Third way
+	colors := make(map[string]string)
+
+	colors["white"] = "#ffffff"
+
+	// Delete keys and values
+	delete(colors, "white")
+
+	fmt.Println(colors)
+
+}
