6216ac6bf6871f84f490dc1db8c8434b2f960b51
Harry Sohal <meghna0593@gmail.com>
2024-04-08 13:18:42 -0700

    adding channels

--- channels/main.go
+++ channels/main.go
@@ -14,16 +14,23 @@ func main() {
 		"http://amazon.com",
 	}
 
+	// Create a channel
+	c := make(chan string)
+
 	for _, url := range urls {
-		checkUrl(url)
+		go checkUrl(url, c) // go routine
 	}
+	fmt.Println(<-c)
 }
 
-func checkUrl(url string) {
+func checkUrl(url string, c chan string) {
+	// _, err := http.Get(url) // blocking call; add Go routines
 	_, err := http.Get(url)
 	if err != nil {
 		fmt.Println(url, "might be down!")
+		c <- "Might be down"
 		return
 	}
 	fmt.Println(url, "is up")
+	c <- "Yes, it's up!"
 }
